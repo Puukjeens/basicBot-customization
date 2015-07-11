bf78528dc17afd7dd3ab8c02dd2977009423ebc4
Puukjeens <klaza@tvnet.lv>
2015-07-11 22:06:20 +0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -119,6 +119,6 @@
     }));
 
     //Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/Yemasthui/basicBot/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/Puukjeens/Heal-Roboti-/master/basicBot.js", extend);
 
 }).call(this);
